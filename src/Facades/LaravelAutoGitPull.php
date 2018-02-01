2787b682131f0cf75808306222540d2c829bff80
Ahmet Imamoglu <ahmet363@gmail.com>
2018-02-02 02:22:59 +0300

    Remove service provider.

--- src/Facades/LaravelAutoGitPull.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ahmeti\LaravelAutoGitPull\Facades;
-
-use Illuminate\Support\Facades\Facade;
-
-class LaravelAutoGitPull extends Facade {
-    protected static function getFacadeAccessor() { return 'laravel-auto-git-pull'; }
-}
---
+++ src/LaravelAutoGitPullController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ahmeti\LaravelAutoGitPull;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LaravelAutoGitPullController extends Controller {
+
+    public function pull(Request $request)
+    {
+        dd('adadsadsasd');
+    }
+}
